99d2a900d021bd7a88d3da3f7268e11e43c0ad73
Helbert Enoch <agredaenoch@gmail.com>
2025-07-27 19:35:49 -0500

    Creación de las clases principales, 'Respuesta', 'Topico', 'Curso', etc

---
+++ src/main/java/com/alurachallenge/Api_ForoHub/domain/curso/Curso.java
@@ -0,0 +1,44 @@
+package com.alurachallenge.Api_ForoHub.domain.curso;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Embeddable
+public class Curso {
+
+    @Column(name = "nombreCurso")
+    private  String nombreCurso;
+    @Column(name = "categoriaCurso")
+    private  String categoriaCurso;
+
+    public Curso(DatosCurso datos) {
+        this.nombreCurso = datos.nombreCurso();
+        this.categoriaCurso = datos.categoriaCurso();
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public String getCategoriaCurso() {
+        return categoriaCurso;
+    }
+
+    public void setCategoriaCurso(String categoriaCurso) {
+        this.categoriaCurso = categoriaCurso;
+    }
+}
